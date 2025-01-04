619351acab62c6fea28056d4b1a7723280c0792e
Land3r <nicolas.gordat@gmail.com>
2025-01-04 18:51:27 +0100

    build: adds base vite path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Required for github pages publication under account.github.io/projectname
+  base: './'
 })
